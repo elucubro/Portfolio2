7948e87a68ae1d3efd71a29eb4eec76787444945
elucubro <114061880+elucubro@users.noreply.github.com>
2024-01-08 12:26:56 +0800

    but it doesnt matter

---
+++ tailwind.config.mjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	darkMode: 'class',
+	content: [
+		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+		'./node_modules/flowbite/**/*.js'
+	],
+	theme: {
+		extend: {
+			colors: {"dark": "#030303"}
+		},
+	},
+	plugins: [
+		require('flowbite/plugin')
+	],
+}
