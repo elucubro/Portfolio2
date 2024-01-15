d64e03e0231369ebbcaf6156e2ab321d80fe28ef
elucubro <114061880+elucubro@users.noreply.github.com>
2024-01-15 16:29:19 +0800

    git push it quick

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,13 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme")
 module.exports = {
-	darkMode: 'class',
 	content: [
 		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
 		'./node_modules/flowbite/**/*.js'
 	],
 	theme: {
 		extend: {
-			colors: {"dark": "#030303"}
+			colors: {"zeus": "#272622", "cucumber": "#7AAA64", "evergreen": "#114D37", "background": "#EFF1E3"},
+			fontFamily: {sans: ["General Sans", ...defaultTheme.fontFamily.sans], serif: ["DM Serif Display", ...defaultTheme.fontFamily.serif]}
 		},
 	},
 	plugins: [
